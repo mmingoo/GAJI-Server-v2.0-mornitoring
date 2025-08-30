f5d2a8b39ecef8e08cafb24cae067ca8eec8b481
mmingoo <aldrn0323@naver.com>
2025-08-30 10:06:34 +0900

    테스트 컨트롤러 수정

--- src/main/java/gaji/service/test/TestController.java
+++ src/main/java/gaji/service/test/TestController.java
@@ -2,8 +2,10 @@ package gaji.service.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/test")
+@RestController
 public class TestController {
 
     @GetMapping("/page")
